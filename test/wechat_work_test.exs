53bf804dca96fb64fa1ec75b457709265658ad42
zhulinpinyu <lixiang.mu@letote.cn>
2019-03-07 17:58:54 +0800

    add simple test

--- test/wechat_work_test.exs
+++ test/wechat_work_test.exs
@@ -2,7 +2,7 @@ defmodule WechatWorkTest do
   use ExUnit.Case
   doctest WechatWork
 
-  test "greets the world" do
-    assert WechatWork.hello() == :world
+  test "fetch token success" do
+    assert {:ok, _} = WechatWork.token()
   end
 end
