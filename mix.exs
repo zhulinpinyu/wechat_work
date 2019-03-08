f47d17bcc6bedff672f781bb299fb49b7be42e0a
zhulinpinyu <lixiang.mu@letote.cn>
2019-03-08 10:24:56 +0800

    reset elixir version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule WechatWork.MixProject do
   def project do
     [
       app: :wechat_work,
-      version: "0.1.0",
-      elixir: "~> 1.8",
+      version: "0.1.1",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env == :prod,
       description: description(),
